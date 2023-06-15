86dbf8c84283355dd9aa45e53c99198a9199c5cd
thisdotEric <siguenzajohneric@gmail.com>
2023-06-15 22:55:17 +0800

    feat: initial setup for git pipe cli file

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	"github.com/thisdotEric/gitxtend/cmd"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	cmd.Execute()
+	// Read input from pipe
+	scanner := bufio.NewScanner(os.Stdin)
+	var output string
+
+	for scanner.Scan() {
+		// Process each line from the pipe
+		line := scanner.Text()
+		// Do any required processing on the input
+		output += line + "\n"
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading standard input:", err)
+		os.Exit(1)
+	}
+
+	// Process the Git output
+	gitOutput := strings.TrimSpace(output)
+	fmt.Println("Git output:", gitOutput)
+
+	// Your additional code logic goes here
 }
